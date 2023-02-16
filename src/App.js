6b7d9804d089b60b76bf62e07e8663b32b05c064
Partha Chakraborty <partha.c1999@gmail.com>
2023-02-16 10:42:25 +0530

    Cards components design completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Analytics from "./components/Analytics";
 import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
       <Hero/>
       <Analytics/>
       <Newsletter/>
+      <Cards/>
     </div>
   );
 }
